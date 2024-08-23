2698ceecf3b2d56fa3872d961cf6b9d65ff10ca7
kshitij33 <kshitij.sawant15770@sakec.ac.in>
2024-08-23 22:44:44 +0530

    added the folder structure

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Contact from './components/Contact'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
+export default App
