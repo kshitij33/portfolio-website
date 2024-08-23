accc0f5035461ac6987e9a4f4943a718b8dc7f1e
kshitij33 <kshitij.sawant15770@sakec.ac.in>
2024-08-23 23:03:44 +0530

    code refactor: added a navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-gray-800 text-white">
+      <div className="flex justify-between items-center p-5 max-w-7xl mx-auto">
+        <div className="text-2xl font-bold cursor-pointer">
+          Logo
+        </div>
+        <ul className="flex space-x-8">
+          <li className="hover:text-gray-400 transition duration-200 cursor-pointer">
+            Home
+          </li>
+          <li className="hover:text-gray-400 transition duration-200 cursor-pointer">
+            Projects
+          </li>
+          <li className="hover:text-gray-400 transition duration-200 cursor-pointer">
+            Skills
+          </li>
+          <li className="hover:text-gray-400 transition duration-200 cursor-pointer">
+            Contacts
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
